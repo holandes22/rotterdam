ac5311c0cf378f84b06bd955b2604255ce05dee4
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-12-02 13:21:11 +0200

    Fixed selected menu item and add toggle menu

--- web/static/app/components/nav-side-menu.js
+++ web/static/app/components/nav-side-menu.js
@@ -2,15 +2,16 @@ import { u } from "umbrellajs";
 
 export default class NavSideMenu {
   constructor(selector) {
-    window.console.log("In nav side Menu");
     this.selector = selector;
     this.menu = u(this.selector);
+    this.layout = u("#layout");
 
     let menuLink = u(`${this.selector} .menu-link`);
 
     menuLink.on("click", evt => {
-      window.console.log("Toggling");
       this.menu.toggleClass("active");
+      this.layout.toggleClass("active");
+      menuLink.toggleClass("active");
     });
 
   }
