cf49ba48f16846d18ca02732cd9baebc093c530c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2017-01-09 14:10:06 +0200

    Updating some packages

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,6 @@ var common = {
         test: /\.js$/,
         exclude: [/node_modules/],
         loader: "babel",
-        query: {
-          presets: ["es2015"]
-        }
       },
       {
         test: /\.css$/,
