fbd1dee9c258cd92699cf7c6a9983b8100e1ad98
Pablo Klijnjan <pabloklijnjan@gmail.com>
2017-01-29 17:01:21 +0200

    Switching to eslint for js code linting

--- web/static/app/app.js
+++ web/static/app/app.js
@@ -1,6 +1,5 @@
 import "phoenix_html";
-import socket from "./socket";
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from "react-tap-event-plugin";
 
 import NavSideMenu from "./components/nav-side-menu";
 import ElmEvents from "../../elm/events/Main.elm";
@@ -9,4 +8,4 @@ injectTapEventPlugin();
 
 ElmEvents.Main.embed(document.getElementById("elm-events"));
 
-const navSideMenu = new NavSideMenu("#nav-side-menu");
+new NavSideMenu("#nav-side-menu");
